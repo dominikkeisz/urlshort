9d4e5282f9ce0847b232a2aababde0fbe6ee1ca9
Dominik Keisz <keisz.d@gmail.com>
2021-08-22 23:57:40 +0200

    Base implementation

---
+++ handler.go
@@ -0,0 +1,16 @@
+package urlshort
+
+import (
+	"net/http"
+)
+
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(rw http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		if dest, ok := pathsToUrls[path]; ok {
+			http.Redirect(rw, r, dest, http.StatusFound)
+			return
+		}
+		fallback.ServeHTTP(rw, r)
+	}
+}
